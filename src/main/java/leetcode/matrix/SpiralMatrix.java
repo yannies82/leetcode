06faf07d9ed03d68b5519684e6707156670d715b
yannies82 <yannies82@gmail.com>
2024-03-14 23:06:42 +0200

    Tweaked solutions, added comments

--- src/main/java/leetcode/matrix/SpiralMatrix.java
+++ src/main/java/leetcode/matrix/SpiralMatrix.java
@@ -12,6 +12,16 @@ public class SpiralMatrix {
 				List.of(1, 2, 3, 4, 8, 12, 11, 10, 9, 5, 6, 7));
 	}
 
+	/**
+	 * Leetcode problem: https://leetcode.com/problems/spiral-matrix. This solution
+	 * traverses the outer sides of the board. When all outer positions are
+	 * traversed, the next layer of outer positions is traversed until there are no
+	 * positions left. Time complexity is O(m * n) where m is the number of rows of
+	 * the board and n is the number of columns of the board.
+	 * 
+	 * @param matrix
+	 * @return
+	 */
 	public static List<Integer> spiralOrder(int[][] matrix) {
 		int hLength = matrix.length;
 		int vLength = matrix[0].length;
@@ -20,23 +30,35 @@ public class SpiralMatrix {
 		int vStart = 0;
 		int vEnd = vLength - 1;
 		List<Integer> result = new ArrayList<>();
+		// for each iteration traverse the outer layer of the board positions
+		// repeat until there are no positions left
 		while (hStart <= hEnd && vStart <= vEnd) {
+			// traverse positions of top row
 			for (int i = vStart; i <= vEnd; i++) {
 				result.add(matrix[hStart][i]);
 			}
+			// traverse positions of last column, excluding the common
+			// position with the first row
 			for (int i = hStart + 1; i <= hEnd; i++) {
 				result.add(matrix[i][vEnd]);
 			}
+			// traverse positions of last row if one exists, excluding the common position
+			// with the last column
 			if (hStart < hEnd) {
 				for (int i = vEnd - 1; i >= vStart; i--) {
 					result.add(matrix[hEnd][i]);
 				}
 			}
+			// traverse positions of first column if one exists, excluding the common
+			// positions
+			// with the last and first rows
 			if (vStart < vEnd) {
 				for (int i = hEnd - 1; i > hStart; i--) {
 					result.add(matrix[i][vStart]);
 				}
 			}
+			// increase index of first row and column, decrease index of last row and column
+			// so that the next iteration continues with the next layer of positions
 			hStart++;
 			hEnd--;
 			vStart++;
--- src/main/java/leetcode/matrix/ValidSudoku.java
+++ src/main/java/leetcode/matrix/ValidSudoku.java
@@ -19,23 +19,43 @@ public class ValidSudoku {
 		check(board2, false);
 	}
 
+	/**
+	 * Leetcode problem: https://leetcode.com/problems/valid-sudoku. This solution
+	 * traverses the sudoku board and keeps track of the numbers in the same row,
+	 * column and rectangular section. If the same number is encountered twice in
+	 * the same row, column or section the board is invalid. Time complexity is O(n
+	 * ^ 2) where n is the number of rows and columns (9).
+	 * 
+	 * @param board
+	 * @return
+	 */
 	public static boolean isValidSudoku(char[][] board) {
 		int length = board.length;
+		// this array keeps the encountered numbers per row, column and section
+		// there are 3 dimensions of tracking (rows, columns, sections), length (9)
+		// number of rows, columns and sections and 10 possible numbers for each
+		// position (0-9)
 		boolean[][][] checks = new boolean[3][length][10];
-		int index;
-		int iSection;
-		int sectionIndex;
+		// traverse all board rows
 		for (int i = 0; i < length; i++) {
-			iSection = 3 * (i / 3);
+			// helps with the calculation of the section index
+			int iSection = 3 * (i / 3);
+			// traverse all board columns
 			for (int j = 0; j < length; j++) {
 				if (board[i][j] == '.') {
+					// ignore dots
 					continue;
 				}
-				index = board[i][j] - '0';
-				sectionIndex = iSection + j / 3;
+				// calculate the index corresponding to the number in the position i, j of the
+				// board
+				int index = board[i][j] - '0';
+				// calculate section index
+				int sectionIndex = iSection + j / 3;
 				if (checks[0][i][index] || checks[1][j][index] || checks[2][sectionIndex][index]) {
+					// return false if this number has appeared again in this row, column or section
 					return false;
 				}
+				// set all 3 indexes (for row, column, section) to true
 				checks[0][i][index] = !checks[0][i][index];
 				checks[1][j][index] = !checks[1][j][index];
 				checks[2][sectionIndex][index] = !checks[2][sectionIndex][index];
