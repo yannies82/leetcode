954892337c4e6ff2abc5f9069bc20c0d0b17b47b
yannies82 <yannies82@gmail.com>
2024-03-31 17:52:15 +0300

    Tweaked solutions, added comments

--- src/main/java/leetcode/graphgeneral/CourseSchedule.java
+++ src/main/java/leetcode/graphgeneral/CourseSchedule.java
@@ -18,6 +18,71 @@ public class CourseSchedule {
 	}
 
 	/**
+	 * Leetcode problem: https://leetcode.com/problems/course-schedule. This
+	 * solution uses an adjacency list and recursively checks for a cycle for all
+	 * courses. Time complexity is O(n + m) where n is the number of courses and m
+	 * is the number of prerequisites.
+	 * 
+	 * @param n
+	 * @param prerequisites
+	 * @return
+	 */
+	public static boolean canFinish(int n, int[][] prerequisites) {
+		// initialize adjacency lists
+		List<List<Integer>> adj = new ArrayList<>();
+		for (int i = 0; i < n; i++) {
+			adj.add(new ArrayList<>());
+		}
+		for (int[] edge : prerequisites) {
+			// for each prerequisite record the course depending on the other one is the
+			// dest and the other course is the source
+			// add an entry with value dest to the adjacency list at index source
+			int source = edge[1];
+			int dest = edge[0];
+			adj.get(source).add(dest);
+		}
+		// initialize visited array to a value which indicates that the course at index
+		// i has not been visited yet
+		int[] visited = new int[n];
+		for (int i = 0; i < n; i++) {
+			visited[i] = -1;
+		}
+		// iterate all courses
+		for (int i = 0; i < n; i++) {
+			// for each non visited course check that a cycle does not exist
+			if (visited[i] == -1 && cycleExists(i, adj, visited)) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	private static boolean cycleExists(int source, List<List<Integer>> adj, int[] visited) {
+		if (visited[source] == 0) {
+			// the course at index source is currently checked for a cycle and is traversed
+			// again, this means that a cycle exists
+			return true;
+		}
+		if (visited[source] == 1) {
+			// the course at index source was checked for a cycle and was not found to
+			// contain one
+			return false;
+		}
+		// mark the index source as currently checked for a cycle
+		visited[source] = 0;
+		for (int dest : adj.get(source)) {
+			// check all adjacent dest nodes to the source for a cycle
+			if (cycleExists(dest, adj, visited)) {
+				return true;
+			}
+		}
+		// the source and all adjacent indexes were found not to contain a cycle
+		// mark source accordingly
+		visited[source] = 1;
+		return false;
+	}
+
+	/**
 	 * This solution uses a map to store the prerequisites per task. It then
 	 * traverses all tasks with prerequisites recursively and tries to find if there
 	 * is a cycle in the prerequisites path.
@@ -26,7 +91,7 @@ public class CourseSchedule {
 	 * @param prerequisites
 	 * @return
 	 */
-	public static boolean canFinish(int numCourses, int[][] prerequisites) {
+	public static boolean canFinish2(int numCourses, int[][] prerequisites) {
 		// populate the prerequisitesMap with all available prerequisites
 		Map<Integer, List<Integer>> prerequisitesMap = new HashMap<>();
 		for (int i = 0; i < prerequisites.length; i++) {
@@ -40,14 +105,14 @@ public class CourseSchedule {
 		boolean[] traversed = new boolean[numCourses];
 		for (Integer courseIndex : prerequisitesMap.keySet()) {
 			// traverse all non visited courses, return false if a course cannot be finished
-			if (!visited[courseIndex] && !canFinishCourse(courseIndex, prerequisitesMap, visited, traversed)) {
+			if (!visited[courseIndex] && !canFinishCourse2(courseIndex, prerequisitesMap, visited, traversed)) {
 				return false;
 			}
 		}
 		return true;
 	}
 
-	private static boolean canFinishCourse(Integer courseIndex, Map<Integer, List<Integer>> prerequisitesMap,
+	private static boolean canFinishCourse2(Integer courseIndex, Map<Integer, List<Integer>> prerequisitesMap,
 			boolean[] visited, boolean[] traversed) {
 		// mark course as visited so that it is not traversed again
 		visited[courseIndex] = true;
@@ -63,13 +128,65 @@ public class CourseSchedule {
 			// cycle exists)
 			// or if it is not visited and cannot be finished (recursively)
 			if (traversed[prerequisite] || (!visited[prerequisite]
-					&& !canFinishCourse(prerequisite, prerequisitesMap, visited, traversed))) {
+					&& !canFinishCourse2(prerequisite, prerequisitesMap, visited, traversed))) {
 				return false;
 			}
 		}
 		// mark course as not traversed, this path has finished
 		traversed[courseIndex] = false;
 		return true;
+	}
+
+	/**
+	 * This solution uses dynamic programming.
+	 * 
+	 * @param numCourses
+	 * @param prerequisites
+	 * @return
+	 */
+	public static boolean canFinish3(int numCourses, int[][] prerequisites) {
+		// populate the prerequisitesMap with all available prerequisites
+		Map<Integer, List<Integer>> prerequisitesMap = new HashMap<>();
+		for (int i = 0; i < prerequisites.length; i++) {
+			// store all prerequisites per course index
+			prerequisitesMap.computeIfAbsent(prerequisites[i][0], k -> new ArrayList<>()).add(prerequisites[i][1]);
+		}
+		// keep the result for each course index
+		Boolean[] dpArray = new Boolean[numCourses];
+		// keep the current traversed courses in this array in order to detect cycles
+		boolean[] traversed = new boolean[numCourses];
+		for (Integer courseIndex : prerequisitesMap.keySet()) {
+			// traverse all non visited courses, return false if a course cannot be finished
+			if (!canFinishCourse3(courseIndex, prerequisitesMap, traversed, dpArray)) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	private static boolean canFinishCourse3(Integer courseIndex, Map<Integer, List<Integer>> prerequisitesMap,
+			boolean[] traversed, Boolean[] dpArray) {
+		if (dpArray[courseIndex] != null) {
+			return dpArray[courseIndex];
+		}
+		List<Integer> prerequisites = prerequisitesMap.get(courseIndex);
+		if (prerequisites == null) {
+			// no prerequisites, no cycle
+			return dpArray[courseIndex] = true;
+		}
+		// mark course as traversed in the current path
+		traversed[courseIndex] = true;
+		for (Integer prerequisite : prerequisites) {
+			// return false if the prerequisite is already traversed in the current path (a
+			// cycle exists)
+			// or if it cannot be finished (recursively)
+			if (traversed[prerequisite] || !canFinishCourse3(prerequisite, prerequisitesMap, traversed, dpArray)) {
+				return dpArray[courseIndex] = false;
+			}
+		}
+		// mark course as not traversed, this path has finished
+		traversed[courseIndex] = false;
+		return dpArray[courseIndex] = true;
 	}
 
 	private static void check(int numCourses, int[][] prerequisites, boolean expected) {
