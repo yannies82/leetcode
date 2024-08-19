0735c322f14a0a8146668513fe29a09c4567e071
yannies82 <yannies82@gmail.com>
2024-08-19 17:43:37 +0300

    Daily challenge solution

---
+++ src/main/java/leetcode/arraystring/TwoKeysKeyboard.java
@@ -0,0 +1,50 @@
+package leetcode.arraystring;
+
+public class TwoKeysKeyboard {
+
+	public static void main(String[] args) {
+		check(3, 3);
+		check(1, 0);
+	}
+
+	/**
+	 * Leetcode problem: https://leetcode.com/problems/2-keys-keyboard. This
+	 * solution greedily checks at each steps if the onScreen characters can be
+	 * exactly divided by the remaining characters and performs copy or paste
+	 * accordingly. Time complexity is O(n).
+	 * 
+	 * @param n
+	 * @return
+	 */
+	public static int minSteps(int n) {
+		if (n == 1) {
+			return 0;
+		}
+		// first two actions are always Copy, Paste
+		int copySize = 1;
+		int actions = 2;
+		int onScreen = 2;
+		while (onScreen < n) {
+			int remaining = n - onScreen;
+			if (remaining % onScreen == 0 && copySize != onScreen) {
+				// the remaining characters can be exactly divided by the onScreen characters
+				// therefore perform copy to increase the copied characters
+				copySize = onScreen;
+			} else {
+				// the remaining characters cannot be exactly divided by the onScreen characters
+				// perform paste because performing copy would make it impossible to reach
+				// exactly n characters
+				onScreen += copySize;
+			}
+			actions++;
+		}
+		return actions;
+	}
+
+	private static void check(int n, int expected) {
+		System.out.println("n is: " + n);
+		System.out.println("expected is: " + expected);
+		int minSteps = minSteps(n); // Calls your implementation
+		System.out.println("minSteps is: " + minSteps);
+	}
+}
