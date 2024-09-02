4ffaeb284c98d106a4f6e38b78a4f7bd02e368c4
yannies82 <yannies82@gmail.com>
2024-09-02 17:42:32 +0300

    Daily challenge solution

---
+++ src/main/java/leetcode/arraystring/FindTheStudentThatWillReplaceTheChalk.java
@@ -0,0 +1,41 @@
+package leetcode.arraystring;
+
+import java.util.Arrays;
+
+public class FindTheStudentThatWillReplaceTheChalk {
+
+	public static void main(String[] args) {
+		check(new int[] { 5, 1, 5 }, 22, 0);
+		check(new int[] { 3, 4, 1, 2 }, 25, 1);
+	}
+
+	/**
+	 * Leetcode problem:
+	 * https://leetcode.com/problems/find-the-student-that-will-replace-the-chalk.
+	 * Time complexity is O(n) where n is the length of the chalk array.
+	 * 
+	 * @param chalk
+	 * @param k
+	 * @return
+	 */
+	public static int chalkReplacer(int[] chalk, int k) {
+		long total = 0;
+		for (int i = 0; i < chalk.length; i++) {
+			total += chalk[i];
+		}
+		long realK = k % total;
+		int i = 0;
+		do {
+			realK -= chalk[i++];
+		} while (realK >= 0);
+		return i - 1;
+	}
+
+	private static void check(int[] chalk, int k, int expected) {
+		System.out.println("chalk is: " + Arrays.toString(chalk));
+		System.out.println("k is: " + k);
+		System.out.println("expected is: " + expected);
+		int chalkReplacer = chalkReplacer(chalk, k); // Calls your implementation
+		System.out.println("chalkReplacer is: " + chalkReplacer);
+	}
+}
